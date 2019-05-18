f16acb360914d4bc7382f77d930dc21c3a33165b
gerryfletch <1997gerry@gmail.com>
2019-05-18 13:48:54 +0100

    Created and documented ConstructorSelectionStrategy interface.

---
+++ src/main/java/dev/testbed/constructors/ConstructorSelectionStrategy.java
@@ -0,0 +1,7 @@
+package dev.testbed.constructors;
+
+/**
+ * Any constructor selector algorithms should implement this class so they can be used for class-specific TestBeds.
+ */
+public interface ConstructorSelectionStrategy {
+}
