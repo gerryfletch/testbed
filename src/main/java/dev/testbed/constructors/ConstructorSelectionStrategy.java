f5197dfcb92a1bb3970568ecd0c27d020542b262
gerryfletch <1997gerry@gmail.com>
2019-05-18 13:53:00 +0100

    Created getConstructor method for selection strategies.

--- src/main/java/dev/testbed/constructors/ConstructorSelectionStrategy.java
+++ src/main/java/dev/testbed/constructors/ConstructorSelectionStrategy.java
@@ -1,7 +1,12 @@
 package dev.testbed.constructors;
 
+import java.lang.reflect.Constructor;
+
 /**
  * Any constructor selector algorithms should implement this class so they can be used for class-specific TestBeds.
  */
 public interface ConstructorSelectionStrategy {
+
+    <T> Constructor<T> getConstructor(Class<T> classUnderTest);
+
 }
